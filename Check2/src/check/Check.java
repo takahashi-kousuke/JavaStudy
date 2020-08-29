54b52e217c5219745fcd3f37297d2db536dcac72
takahashi-kousuke <takahashi-kousuke@users.noreply.github.com>
2020-08-30 00:54:11 +0900

    2章　チェックテスト
    2章　チェックテスト

---
+++ Check2/src/check/Check.java
@@ -0,0 +1,23 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+
+       private static String firstName = "TAKAHASHI";
+       private static String lastName = "KOUSUKE";
+
+
+    private static void printName(String firstName , String lastName) {
+        System.out.println("printNameメソッド→"+firstName+lastName);
+    }
+    public static void main (String[] args) {
+        printName(firstName,lastName);
+        
+    Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+    pet.introduce();
+    RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE  );
+    robotpet.introduce();
+    }
+
+}
