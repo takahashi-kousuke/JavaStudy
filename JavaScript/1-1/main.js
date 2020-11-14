1baca95eb0d27b41dc742c46f3b3f0009ccd6190
takahashi-kousuke <takahashi-kousuke@users.noreply.github.com>
2020-11-14 14:34:13 +0900

    Java Script 1-1　課題提出
    Java Script 1-1　課題提出

---
+++ JavaScript/1-1/main.js
@@ -0,0 +1,9 @@
+console.log("Hello World!!");
+console.log(65536);
+console.log(10 + 5);
+console.log(10 * 5);
+console.log("Hello World!!");
+console.log("Hello " + "World!!");
+console.log("10"+"+"+" 5");
+console.log(10 + 8);
+console.log("20割る3のあまりは"+ 20%3);
