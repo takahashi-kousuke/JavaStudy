3d12e8f541e8ae92dfd9d9550dc7f7de7fc4dd80
takahashi-kousuke <takahashi-kousuke@users.noreply.github.com>
2020-11-16 19:48:17 +0900

    JavaScript 1-6 課題提出
    JavaScript 1-6 課題提出

--- JavaScript/1-6/main.js
+++ JavaScript/1-6/main.js
@@ -25,9 +25,16 @@ console.log(ORESAMA.name);
 //問1：10、15、20、25を含む配列の変数scoresを用意して、
 //画像のように偶数だけを出力してください。
 let scores = [10,15,20,25];
-console.log(scores);
-console.log(scores[0]+"は偶数です");
-console.log(scores[2]+"は偶数です");
+//console.log(scores);
+//console.log(scores[0]+"は偶数です");
+//console.log(scores[2]+"は偶数です");
+
+for(let i = 0;i < scores.length;i++){
+if(scores[i] % 2 == 0){
+    console.log(scores[i]+"は偶数です")
+}
+
+}
 
 
 
