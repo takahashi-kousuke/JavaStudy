4cb2fc0e884b145151204667bd4ed34839a9ab83
takahashi-kousuke <takahashi-kousuke@users.noreply.github.com>
2020-11-16 21:11:44 +0900

    JavaScript 1-4 課題提出
    JavaScript 1-4 課題提出

--- JavaScript/1-4/main.js
+++ JavaScript/1-4/main.js
@@ -41,18 +41,17 @@
 
 let number = 1;
 for(let number = 1 ; number <= 100; number++){
-    if(number%3===0){
-        console.log("Fizz!");
-    }
-    if(number%5===0){
-        console.log("Buzz!");
-    }
     if(number%3===0 && number%5===0){
         console.log("FizzBuzz!!");
-        
+    }else if(number%5===0){
+        console.log("Buzz!");
+    }else if(number%3===0){
+        console.log("Fizz!");
     }else if(number%3!==0 && number%5!==0){
         console.log(number);
-        
+    
     }
 
 }
+
+
